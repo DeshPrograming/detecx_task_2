a4d0cd1a417ca120258e1cc8a9ab0f4211e47262
Desh Piyumantha <121693916+DeshPrograming@users.noreply.github.com>
2023-02-22 13:19:09 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>De'tec-x Task</title>
+
+    <link rel="icon" href="assets/img/favicon-32x32.png" />
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="font-awesome.css">
+    <link rel="stylesheet" href="font-awesome.min.css">
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <div class="mycontent">
+            <div class="col-12">
+                <h1 class="header">Join our community</h1>
+            </div>
+            <div class="col-12">
+                <h3 class="sub-header">30-day, hassle-free money back guarantee</h3>
+            </div>
+            <div class="col-12">
+                <p class="paragraph">Gain access to our full library of tutoriais along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 col-lg-6 box1">
+                <div class="mycontent">
+                    <div class="col-12">
+                        <h3 class="box-header">Monthly Subscription</h3>
+                    </div>
+                    <div class="row">
+                        <div class="col-3">
+                            <h1 class="price">$29</h1>
+                        </div>
+                        <div class="col-9 ">
+                            <h3 class="sub-header-price">Per Month</h3>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <p class="paragraph2">Full access for less than 1$ a day</p>
+                    </div>
+                    <div class="col-12">
+                        <button class="col-12 button">Sign Up</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6 box2">
+                <div class="mycontent">
+                    <div class="col-12">
+                        <h3 class="box-header">Why Us</h3>
+                    </div>
+                    <div class="col-12">
+                        <p class="point">Tutorials by industry exprts <br>Peer & expert code review <br>Coding exercises <br>Access to our GitHub repos<br>Community forum <br>Flashcard decks<br>New videos every week </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <script src="script.js"></script>
+    <script src="bootstrap.js"></script>
+</body>
+
+</html>
